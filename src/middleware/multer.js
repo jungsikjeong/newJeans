2ba69c0f1c4ec6273f701f64d1a9903da99c4f4a
JungSik Jeong <wndtlr1024@gmail.com>
2023-02-26 12:41:58 +0900

    multer 에러 해결

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -2,17 +2,15 @@ const multer = require('multer');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    console.log('file', file);
-    // cb(null, +'uploads/');
-    cb(null, __dirname.slice(0, 53) + 'public/image');
+    cb(null, __dirname.slice(0, 47) + 'uploads');
   },
 
   filename: function (req, file, cb) {
     file.originalname = Buffer.from(file.originalname, 'latin1').toString(
       'utf8'
     );
-    console.log('file:::', file);
-    cb(null, file.originalname + '-' + Date.now().toString());
+    S;
+    cb(null, Date.now().toString() + '-' + file.originalname);
   },
 });
 
